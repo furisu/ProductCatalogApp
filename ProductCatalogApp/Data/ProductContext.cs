6528add777ef62d2647e6e47efacd9ee45ba4a1e
DESKTOP-V0N2EIE\okawa <okawafris@gmail.com>
2025-07-05 16:03:51 +0900

    Scaffold自動生成

---
+++ ProductCatalogApp/Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogApp.Models;
+
+namespace ProductCatalogApp.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductCatalogApp.Models.Product> Product { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=productcatalog.db");
+    }
+
+
+}
---
+++ ProductCatalogApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCatalogApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // 主キー（自動採番）
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
